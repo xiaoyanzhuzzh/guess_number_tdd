33684c75f778f745542742b00a1c9a23fd5a710c
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 15:20:13 +0800

    finish guessIntegrationTest

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/GuessIntegrationTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/GuessIntegrationTest.java
@@ -1,7 +1,21 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class GuessIntegrationTest {
+    @Test
+    public void should_be_right(){
+        Random random = mock(Random.class);
+        when(random.nextInt(10)).thenReturn(1, 2, 3, 4);
+
+        Guess guess = new Guess(new AnswerGenerator(random), new CompareNumber());
+        String input = "1234";
+        assertThat(guess.guess(input)).isEqualTo("4A0B");
+    }
 }
