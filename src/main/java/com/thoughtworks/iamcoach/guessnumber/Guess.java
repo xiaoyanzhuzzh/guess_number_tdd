752f9187ca6a4fce0049124357fb86e9316e4514
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 14:54:02 +0800

    finish guess method

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/Guess.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/Guess.java
@@ -4,4 +4,16 @@ package com.thoughtworks.iamcoach.guessnumber;
  * Created by zhangzhihui on 14-11-1.
  */
 public class Guess {
+    private CompareNumber compareNumber;
+    private AnswerGenerator answerGenerator;
+
+    public Guess(AnswerGenerator answerGenerator, CompareNumber compareNumber) {
+        this.answerGenerator = answerGenerator;
+        this.compareNumber = compareNumber;
+    }
+
+    public String guess(String input) {
+        String answer = answerGenerator.generate();
+        return compareNumber.compare(input, answer);
+    }
 }
