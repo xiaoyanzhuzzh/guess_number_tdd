d73242e305061ef2db1ea92bf02f5de238c1f9a6
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 15:20:48 +0800

    finish unit test GuessTest

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/GuessTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/GuessTest.java
@@ -1,26 +1,13 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Random;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class GuessTest {
-    //集成测试
-    @Test
-    public void should_be_right(){
-          Random random = mock(Random.class);
-          when(random.nextInt(10)).thenReturn(1, 2, 3, 4);
 
-        Guess guess = new Guess(new AnswerGenerator(random), new CompareNumber());
-        String input = "1234";
-        assertThat(guess.guess(input)).isEqualTo("4A0B");
-    }
-    //单元测试
     @Test
     public void can_get_correct_output(){
         AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
