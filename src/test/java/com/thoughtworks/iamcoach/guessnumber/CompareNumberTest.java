c1abbc36cf2e25f83120216479e132b37d8f4b76
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 13:26:04 +0800

    finish should_return_0A0B

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
@@ -8,10 +8,19 @@ public class CompareNumberTest {
     @Test
     public void length_should_be_4(){
         CompareNumber compareNumber = new CompareNumber();
-        
+
         String input = "1234";
         String answer = "1234";
 
         assertThat(compareNumber.compare(input, answer).length()).isEqualTo(4);
+    }
+
+    @Test
+    public void should_return_0A0B(){
+        CompareNumber compareNumber = new CompareNumber();
+        String input = "1234";
+        String answer = "5678";
+
+        assertThat(compareNumber.compare(input, answer)).isEqualTo("0A0B");
     }
 }
