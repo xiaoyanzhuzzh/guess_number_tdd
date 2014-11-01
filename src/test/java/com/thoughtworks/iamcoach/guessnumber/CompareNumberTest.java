e7562cd67b16247cc4f08cb2189c7c6b347ccae5
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 13:23:05 +0800

    finish length_should_be_4 test

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
@@ -1,7 +1,17 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CompareNumberTest {
+    @Test
+    public void length_should_be_4(){
+        CompareNumber compareNumber = new CompareNumber();
+        
+        String input = "1234";
+        String answer = "1234";
+
+        assertThat(compareNumber.compare(input, answer).length()).isEqualTo(4);
+    }
 }
