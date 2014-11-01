47d9673b7593e2cf099c5b4f3cd7a2242471a95c
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 14:37:31 +0800

    finish generate method

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
@@ -1,13 +1,26 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
+import java.util.Random;
+
 public class AnswerGenerator {
+    private  Random random;
+
+    AnswerGenerator(Random random){
+        this.random = random;
+    }
+
     public String generate() {
         String answers = "";
-        for(int i = 0; i < 4; i++){
-            int answer = (int)(10 * Math.random());
+
+        while (answers.length() < 4){
+            int answer = random.nextInt(10);
+
+            if(answers.contains(answer +"")){
+                answer = random.nextInt(10);
+            }
+
             answers += answer;
         }
-
         return answers;
     }
 }
