21d1e067df4f8336260ddeba484007522f3e0717
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 13:32:49 +0800

    finish compare method

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/CompareNumber.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/CompareNumber.java
@@ -4,4 +4,22 @@ package com.thoughtworks.iamcoach.guessnumber;
  * Created by zhangzhihui on 14-11-1.
  */
 public class CompareNumber {
+    public String compare(String input, String answer) {
+        int numberA = 0;
+        int numberB = 0;
+
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == answer.charAt(i)){
+                numberA += 1;
+            }else{
+                for(int j = 0; j < answer.length(); j++){
+                    if(input.charAt(i) == answer.charAt(j)){
+                        numberB += 1;
+                    }
+                }
+            }
+        }
+
+        return numberA + "A" + numberB + "B";
+    }
 }
